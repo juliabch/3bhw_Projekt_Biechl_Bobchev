a2f4313c74002110fb67c2d9c4266c5c57a517a4
juliabch <biechl.julia@gmail.com>
2022-03-04 13:12:44 +0100

    Subject class

--- Projekt_Biechl_Bobchev/src/Models/Teacher.java
+++ Projekt_Biechl_Bobchev/src/Models/Teacher.java
@@ -1,10 +1,11 @@
 package Models;
 
-import javax.security.auth.Subject;
 import java.util.*;
 import java.time.LocalDate;
 
 public class Teacher extends Person{
 
-    private String Subject;
+
+    private boolean hasClass;
+    private List<Subject>
 }
