4abef2725e6d3124fcf784edbb52423c6c4c7a06
juliabch <biechl.julia@gmail.com>
2022-03-11 13:02:31 +0100

    Student, Teacher, Subject classes finished

--- Projekt_Biechl_Bobchev/src/Models/Subject.java
+++ Projekt_Biechl_Bobchev/src/Models/Subject.java
@@ -1,4 +1,32 @@
 package Models;
 
 public class Subject {
+
+    private int subject_id;
+    private String subject;
+
+    public int getSubjectId() {
+        return subject_id;
+    }
+
+    public void setSubjectId(int subject_id) {
+        this.subject_id = subject_id;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public Subject(){
+        this(0, " ");
+    }
+
+    public Subject(int id, String subject){
+        this.setSubjectId(id);
+        this.setSubject(subject);
+    }
 }
--- Projekt_Biechl_Bobchev/src/Models/Teacher.java
+++ Projekt_Biechl_Bobchev/src/Models/Teacher.java
@@ -1,11 +1,50 @@
 package Models;
 
-import java.util.*;
 import java.time.LocalDate;
+import java.util.*;
 
 public class Teacher extends Person{
 
 
-    private boolean hasClass;
-    private List<Subject>
+    private boolean _isFormTeacher;
+    private List<Subject> _subjects = new ArrayList<>();
+
+    public boolean getIsFormTeacher() {
+        return _isFormTeacher;
+    }
+
+    public void setIsFormTeacher(boolean formTeacher) {
+        _isFormTeacher = formTeacher;
+    }
+    public Teacher(){
+        this(0, " ", " ", LocalDate.MIN, " ", Gender.notSpecified, false);
+    }
+
+    public Teacher(int id, String fn, String ln, LocalDate bd, String mA, Gender gr, Boolean ft){
+        super(id, fn, ln, bd, mA, gr);
+        this.setIsFormTeacher(ft);
+
+    }
+
+    public boolean addSubject(Subject subject){
+        if(subject != null){
+            return this._subjects.add(subject);
+        }
+        return false;
+    }
+    public boolean removeSubject(int subjectToDelete){
+        for(Subject s : this._subjects){
+            if(s.getSubjectId() == subjectToDelete){
+                this._subjects.remove(s);
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public String toString(){
+        return super.toString() + " " + this._isFormTeacher + " " + this._subjects;
+    }
+
+
 }
